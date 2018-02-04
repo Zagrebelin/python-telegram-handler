2b20bf9bb123200e68be7be85ab8bbeab7bd99e2
Pavel Zagrebelin <pavel@zagrebelin.ru>
2018-02-05 01:05:16 +0500

    Correct formatting.

--- telegram_handler/formatters.py
+++ telegram_handler/formatters.py
@@ -44,7 +44,6 @@ class HtmlFormatter(TelegramFormatter):
         """
         :param logging.LogRecord record:
         """
-        super(HtmlFormatter, self).format(record)
 
         if record.funcName:
             record.funcName = escape_html(str(record.funcName))
@@ -60,7 +59,7 @@ class HtmlFormatter(TelegramFormatter):
             else:
                 record.levelname += ' ' + EMOJI.RED_CIRCLE
 
-        return self.fmt % record.__dict__
+        return super(HtmlFormatter, self).format(record)
 
     def formatException(self, *args, **kwargs):
         string = super(HtmlFormatter, self).formatException(*args, **kwargs)
